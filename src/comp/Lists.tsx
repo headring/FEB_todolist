224ed566917f002581711fe6db340c756a4fa595
headring <myhakiki@gmail.com>
2022-10-25 23:26:19 +0900

    feat: list 컴포넌트 불러오기

--- src/comp/Lists.tsx
+++ src/comp/Lists.tsx
@@ -1,29 +1,16 @@
-import Loading from "./Loading";
-import useFetch from "../hooks/useFetch";
+import useFetch, { Data } from "../hooks/useFetch";
+import List from "./List";
 
 // 노마드 리액트 유료강의 TS
-
-export interface List{
-  id: number;
-  title: string;
-  contents: string;
-}
 
 const Lists = () => {
   const [data, isPending, error] = useFetch("http://localhost:4000/list/");
 
 
   return(
-    <div>
-    {isPending && <Loading />}
-    {data && data.map(el => (
-      <div key={el.id}>
-        <h2>{el.title}</h2>
-        <p>{el.contents}</p>
-      </div>
-    ))}
-  </div>
-
+    <>
+      <List data={data} isPending={isPending}/>
+    </>
   )
 
 }
